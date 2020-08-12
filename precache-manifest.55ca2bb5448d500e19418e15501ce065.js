644e8f2968a49a4e2244e46c56886f226dc5b2cd
Omid Parsa <omidparsa.1985@gmail.com>
2020-08-12 06:23:59 +0200

    Updates

---
+++ precache-manifest.55ca2bb5448d500e19418e15501ce065.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23aa961e17432b9f5ad0884edcf3c62a",
+    "url": "/food-recipe/index.html"
+  },
+  {
+    "revision": "c11f62bf8539af470c15",
+    "url": "/food-recipe/static/css/main.ef13f231.chunk.css"
+  },
+  {
+    "revision": "c14b72d6009428b52d54",
+    "url": "/food-recipe/static/js/2.6f054730.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-recipe/static/js/2.6f054730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c11f62bf8539af470c15",
+    "url": "/food-recipe/static/js/main.6e4464c4.chunk.js"
+  },
+  {
+    "revision": "779335723206f8e70329",
+    "url": "/food-recipe/static/js/runtime-main.3dc38f30.js"
+  }
+]);
